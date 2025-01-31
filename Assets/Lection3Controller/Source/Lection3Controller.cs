ade4c04b97f10fc5ba685121667190e34b91765a
Roman <chmile@ukr.net>
2025-01-31 22:19:41 +0400

    [ADD] Lesson_2 ver_2

--- Assets/Lection3Controller/Source/Lection3Controller.cs
+++ Assets/Lection3Controller/Source/Lection3Controller.cs
@@ -11,22 +11,44 @@ namespace Lection3Controller.Source
         [ContextMenu("Print")]
         private void Print()
         {
-            if (list.Count == 0)
+            string msg = "List:";
+            for (int i = 0; i < list.Count; ++i)
+                msg += $"\n{list[i]}";
+            Debug.Log(msg);
+        }
+
+        [ContextMenu("Add")]
+        private void Add()
+        {
+            list.Add(value);
+            Debug.Log($"Added: {value}");
+        }
+
+        [ContextMenu("Remove")]
+        private void Remove()
+        {
+            for (int i = list.Count - 1; i >= 0; i--)
             {
-                Debug.Log("List is empty.");
-            }
-            else
-            {
-                string msg = "List:\n" + string.Join("\n", list);
-                Debug.Log(msg);
-            }
+                if (list[i] == value)
+                {
+                    list.RemoveAt(i);
+                    Debug.Log($"Removed: {value} at position {i}");
+                }
+            } 
         }
 
         [ContextMenu("Clear")]
-        private void ClearList()
+        private void Clear()
         {
             list.Clear();
             Debug.Log("Cleared.");
+        }
+        
+        [ContextMenu("Sort")]
+        private void Sort()
+        {
+            list.Sort();
+            Debug.Log("Sorted.");
         }
     }
 }
